59e08ce19ff25a86cda364102ac86dcd7c9c6f15
bharateeshca <bharateeshwork@gmail.com>
2026-08-10 22:48:31 +0530

    intro_concepts commit

---
+++ intro.py
@@ -0,0 +1,29 @@
+# # Global
+# def sum():
+#     # Local
+#     c = 3
+#     return c
+
+# class Karthik:
+#     def __init__(self):
+#         self.c = 0
+
+#     def setter(self, value):
+#         self.c = value
+
+#     def getter(self):
+#         return self.c
+
+# k = Karthik()
+# k.setter(5)
+# c = k.getter()
+# print(c)
+
+def sum():
+    c=3
+    print(c)
+    return c
+
+c = sum()
+
+print(c)
